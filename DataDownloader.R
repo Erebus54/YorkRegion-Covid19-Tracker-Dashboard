7a79601b826942ee1e0c38db19f78da775cbdc65
Erebus54 <patrick.schnurbusch@gmail.com>
2020-10-25 00:45:40 -0400

    first commit

---
+++ DataDownloader.R
@@ -0,0 +1,48 @@
+library(lubridate)
+library(data.table)
+library(dplyr)
+library(beepr)
+
+setwd("C:/Users/patri/Documents/Github/YorkRegion-Covid19-Tracker-Dashboard/")
+
+#grab file 
+loc <- "https://data.ontario.ca/dataset/f4112442-bdc8-45d2-be3c-12efae72fb27/resource/455fd63b-603d-4608-8216-7d8647f43350/download/conposcovidloc.csv"
+casestatus <- fread(input = loc, encoding = 'UTF-8',data.table = F)
+
+#data dict URL 
+#https://data.ontario.ca/dataset/status-of-covid-19-cases-in-ontario
+#end_time <- Sys.time()
+#end_time - start_time
+
+
+#clean CaseStatus file
+casestatus$Accurate_Episode_Date <- as.Date(casestatus$Accurate_Episode_Date)
+casestatus$Case_Reported_Date <- as.Date(casestatus$Case_Reported_Date)
+casestatus$Test_Reported_Date <- as.Date(casestatus$Test_Reported_Date)
+
+target <- "York Region Public Health Services"
+
+casestatus <- casestatus %>% 
+  dplyr::filter(Reporting_PHU == target)
+
+#create .csv for each date of dataset
+dirPath <- paste(getwd(), "/datasets/", sep = "")
+
+#Delete files if exists 
+do.call(file.remove, list(list.files(dirPath, full.names = TRUE)))
+fileName = paste(dirPath, 'casestatus.csv',sep = '')
+#Write Case Status File 
+write.csv(casestatus, 
+          file = fileName, 
+          fileEncoding = 'UTF-8', 
+          row.names = F)
+
+print(paste("casestatus.csv has been dowloaded",
+            nrow(casestatus), "Rows x ", ncol(casestatus), "Columns",  
+            sep = " ")
+)
+
+beep(sound = 1, expr = NULL)
+
+print(paste("Download Completed"))
+remove(list = ls())
